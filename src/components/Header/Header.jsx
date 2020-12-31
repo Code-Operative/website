f206ccee8b3e5a2517dc999517e067a02d5fb995
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-12-31 16:52:03 +0000

    added header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import style from './../../style/components/header.module.css';
+
+const Header = () => {
+
+	return (
+		<div>
+			<div className={style.headerContainer}>
+				<div>page 1</div>
+				<div>page 2</div>
+				<div>page 3</div>
+			</div>
+		</div>
+
+	);
+};
+
+export default Header;
