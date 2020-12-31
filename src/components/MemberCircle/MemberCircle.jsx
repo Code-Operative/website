6681bf12832dea50a328082e724a0aa3786c5c6c
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-12-31 16:52:31 +0000

    added member component

---
+++ src/components/MemberCircle/MemberCircle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import style from './../../style/components/member.module.css';
+
+const Member = () => {
+
+    return (
+        <div className={style.circleContainer}>
+            this is one circle
+        </div>
+    );
+};
+
+export default Member;
