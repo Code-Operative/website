bb216031c67a099e3404c59df9c967ef6dee71d4
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2020-12-31 16:52:53 +0000

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './../components/Header/Header';
+
+export default function About() {
+	return (
+		<div>
+			<Header />
+			<div> This is the header page</div>
+		</div>
+	);
+}
+
