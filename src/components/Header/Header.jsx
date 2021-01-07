449407156a2c5bfa3d50010f6a4e4f0ea059b67c
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2021-01-07 22:45:33 +0000

    fill header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from "gatsby";
 import style from './../../style/components/header.module.css';
 
 const Header = () => {
@@ -6,12 +7,27 @@ const Header = () => {
 	return (
 		<div>
 			<div className={style.headerContainer}>
-				<div>page 1</div>
-				<div>page 2</div>
-				<div>page 3</div>
+				<div> Code-Operative </div>
+				<div className={style.navbar}>
+					<div className={style.navbar}>
+						<Link to="/" className={style.link}> Home </Link>
+					</div>
+					<div className={style.navbar}>
+						<Link to="/members" className={style.link}> Member </Link>
+					</div>
+					<div className={style.navbar}>
+						<Link to="/projects" className={style.link}> Project </Link>
+					</div>
+					<div className={style.navbar}>
+						<Link to="/members" className={style.link}> Find Us </Link>
+					</div>
+					<div className={style.navbar}>
+						<Link to="/members" className={style.link}> Blog </Link>
+					</	div>
+				</div>
+				<div>Join Us</div>
 			</div>
 		</div>
-
 	);
 };
 
