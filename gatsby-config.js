db11e0d70f7ec97a9474627f5dd184cac4871dcf
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2021-01-16 14:15:09 +0000

    add blog inline images with gatsby-remark-images

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,7 +29,19 @@ module.exports = {
         path: `${__dirname}/src/data`,
       },
     },
-    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 600,
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-transformer-json`,
