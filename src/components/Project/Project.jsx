b43a0a0dc483ab3c59c9ba3a093f222edda0297d
Ken Joo Yeap <39419492+kyeap@users.noreply.github.com>
2021-01-30 10:58:42 +0000

    build project slide effect

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
+import Carousel from './carousel';
 
 export default function Project(props) {
 
-  const { imageURL, name } = props;
+  const { imageURL, name, description, color } = props;
 
   const data = useStaticQuery(graphql`
     {
@@ -20,13 +21,13 @@ export default function Project(props) {
   `)
 
   const image = data.allFile.edges.find(n => n.node.publicURL.includes(imageURL));
-
+  console.log(image);
   // if (image == undefined) {
-  const projectComponentStyle = {
-    width: "100%",
-    height: "400px",
-    backgroundImage: 'url(' + image.node.publicURL + ')'
-  };
+  // const projectComponentStyle = {
+  //   width: "100%",
+  //   height: "400px",
+  //   backgroundImage: 'url(' + image.node.publicURL + ')'
+  // };
   // } else {
   //   const projectComponentStyle = {
   //     width: "100%",
@@ -36,8 +37,21 @@ export default function Project(props) {
 
   return (
     <React.Fragment>
-      <div style={projectComponentStyle}>{name}</div>
-      {image ? <img alt="A code-operative project" src={image.node.publicURL} /> : <img alt="A code-operative project" />}
+      {/* <div style={projectComponentStyle}>{name}</div>
+      {image ? <img alt="A code-operative project" src={image.node.publicURL} /> : <img alt="A code-operative project" />} */}
+      <Carousel>
+        {image && <img src={image.node.publicURL} alt="A code-operative project" />}
+        <div className="projectDescriptonContainer" style={{ backgroundColor: color }}>
+          <div className="projectDescriptonContainer_wrapper">
+            <div className="projectDescriptonTitle">{name}</div>
+            {description.map((paragraph) => {
+              return (
+                <div className="projectDescripton">{paragraph}</div>
+              )
+            })}
+          </div>
+        </div>
+      </Carousel>
     </React.Fragment>
 
   )
--- src/data/project.js
+++ src/data/project.js
@@ -1,11 +1,29 @@
 export const projectsList = [
 	{
-		name: 'Gemma Rose Foundation',
-		image: 'header'
+		name: 'Twine',
+		image: 'Twine.png',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo turpis, feugiat eget odio sit amet, tempor mollis erat. Vivamus magna quam, bibendum ut massa ac, placerat iaculis tellus. Nulla et gravida ipsum. Integer porttitor lacus nunc, in pretium mauris sodales quis. Aliquam sit amet augue non sapien ultrices tempor. Aliquam a enim ut lorem suscipit semper nec sed justo. Phasellus dapibus molestie est sit amet accumsan. Quisque sed finibus velit. Pellentesque nibh lorem, sodales quis vestibulum ac, gravida a est.',
+			'Curabitur enim nunc, aliquet vel felis sed, tempus congue enim. Quisque pharetra ac lectus ac dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed nec vehicula ipsum. Aenean lorem sapien, tincidunt quis massa vitae, porta hendrerit justo. Donec quis nisl ut nisi molestie fermentum. Vestibulum a enim hendrerit, viverra orci sit amet, feugiat elit. Vestibulum sit amet mauris sit amet sapien sodales elementum. Duis ut tellus id leo vehicula eleifend. Mauris nec volutpat est. Maecenas sed venenatis ante. Proin commodo sapien vitae justo pellentesque tincidunt.'
+		],
+		color: '#ffb000'
 	},
 	{
-		name: 'Twine',
-		image: 'lalal'
+		name: 'Acorn',
+		image: 'Acorn.png',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo turpis, feugiat eget odio sit amet, tempor mollis erat. Vivamus magna quam, bibendum ut massa ac, placerat iaculis tellus. Nulla et gravida ipsum. Integer porttitor lacus nunc, in pretium mauris sodales quis. Aliquam sit amet augue non sapien ultrices tempor. Aliquam a enim ut lorem suscipit semper nec sed justo. Phasellus dapibus molestie est sit amet accumsan. Quisque sed finibus velit. Pellentesque nibh lorem, sodales quis vestibulum ac, gravida a est.',
+			'Curabitur enim nunc, aliquet vel felis sed, tempus congue enim. Quisque pharetra ac lectus ac dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed nec vehicula ipsum. Aenean lorem sapien, tincidunt quis massa vitae, porta hendrerit justo. Donec quis nisl ut nisi molestie fermentum. Vestibulum a enim hendrerit, viverra orci sit amet, feugiat elit. Vestibulum sit amet mauris sit amet sapien sodales elementum. Duis ut tellus id leo vehicula eleifend. Mauris nec volutpat est. Maecenas sed venenatis ante. Proin commodo sapien vitae justo pellentesque tincidunt.'
+		],
+		color: '#f84a4b'
+	},
+	{
+		name: 'Land Explorer',
+		image: 'LX.png',
+		description: [
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed justo turpis, feugiat eget odio sit amet, tempor mollis erat. Vivamus magna quam, bibendum ut massa ac, placerat iaculis tellus. Nulla et gravida ipsum. Integer porttitor lacus nunc, in pretium mauris sodales quis. Aliquam sit amet augue non sapien ultrices tempor. Aliquam a enim ut lorem suscipit semper nec sed justo. Phasellus dapibus molestie est sit amet accumsan. Quisque sed finibus velit. Pellentesque nibh lorem, sodales quis vestibulum ac, gravida a est.',
+			'Curabitur enim nunc, aliquet vel felis sed, tempus congue enim. Quisque pharetra ac lectus ac dictum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed nec vehicula ipsum. Aenean lorem sapien, tincidunt quis massa vitae, porta hendrerit justo. Donec quis nisl ut nisi molestie fermentum. Vestibulum a enim hendrerit, viverra orci sit amet, feugiat elit. Vestibulum sit amet mauris sit amet sapien sodales elementum. Duis ut tellus id leo vehicula eleifend. Mauris nec volutpat est. Maecenas sed venenatis ante. Proin commodo sapien vitae justo pellentesque tincidunt.'
+		],
+		color: '#86ff00'
 	}
-
 ];
